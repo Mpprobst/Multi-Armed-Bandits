b3735c836efa1ecc96c05a88a111c3cace903dcb
Michael Probst <mpprobst@gmail.com>
2020-09-10 13:38:06 -0400

    fix eps greedy

--- bandits/epsGreedyAgent.py
+++ bandits/epsGreedyAgent.py
@@ -10,8 +10,7 @@ import argparse
 #Okay what do we need to do. First, create agents. This should use command line args.
 #Also we need to create the game
 class epsGreedyAgent:
-    eps = 0.5 #need a good value for epsilon
-
+    eps = 0.5
     def __init__(self):
         #self.currentState.print_board()
         self.name = "Eric the Epsilon Greedy Agent"
@@ -21,6 +20,12 @@ class epsGreedyAgent:
     history is an array with format [arm_pulled, reward]
     """
     def recommendArm(self, bandit, history):
+        explorationProb = random.random()
+        if explorationProb < self.eps:
+            #explore
+            return random.randint(0, bandit.getNumArms()-1)
+
+        #else exploit
         #Look at each arm
         #calculate best looking arm
         armRewards = []
@@ -36,33 +41,15 @@ class epsGreedyAgent:
             #print(f'arm {arm} pulled {timesArmPulled} times for total reward of {cumRewardForArm}')
             if (timesArmPulled > 0):
                 #Q_hat = 1/timesPulledCurrentArm * SUM(reward_at_time_t * (a_tau == a))
-                q_hat = (1 / timesArmPulled) * cumRewardForArm
+                q_hat = cumRewardForArm / timesArmPulled
                 armRewards.append((arm, q_hat))
 
         #Best looking arm is the one with the largest Q_hat
         if len(armRewards) > 0:
-            q_max = list(map(max, zip(*armRewards)))
+            q_max = armRewards[0]
+            for q in armRewards:
+                if q[1] > q_max[1]:
+                    q_max = q
+            return q_max[0]
 
-            #Calculate weighted probability of each arm
-            k = 1.0
-            probabilities = []
-            for arm in range(bandit.getNumArms()):
-                #if: arm is the best looking arm, then its probability = 1 - eps + eps/k
-                if arm == q_max[0]:
-                    probabilities.append(1-self.eps + (self.eps/k))
-                #else: probability = eps/k
-                else:
-                    probabilities.append(self.eps/k)
-                    #what is k?
-
-            randNum = random.uniform(0, sum(probabilities))
-            #print(probabilities)
-            #print(f'randNum = {randNum} from range (0, {sum(probabilities)})')
-            for i in range(len(probabilities)):
-                #print(f'i = {i} prob = {probabilities[i]} random = {randNum}')
-                if probabilities[i] > randNum:
-                    #print(f'choosing {i}')
-                    return i
-                else:
-                    randNum -= probabilities[i]
         return False
